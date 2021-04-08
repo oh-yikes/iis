9503cf3d130c77b5821e4043066083ca55332a4f
Darya Volokhova <71918075+oh-yikes@users.noreply.github.com>
2021-04-08 22:39:35 +0500

    Семантическая сеть

---
+++ lab3/SemNet/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace setee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вопрос:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(22, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ответ:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(22, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(527, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "?";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(50, 98);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(133, 26);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged_1);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(382, 98);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(133, 26);
+            this.comboBox4.TabIndex = 8;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(22, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "1.";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(189, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(187, 18);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "относится к категории";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(382, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 18);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "?";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(239, 144);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(133, 26);
+            this.comboBox5.TabIndex = 14;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(50, 185);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(133, 26);
+            this.comboBox6.TabIndex = 13;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(22, 144);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(162, 18);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "2. Есть ли шерсть у";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(22, 188);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 18);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "3.";
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox7.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Location = new System.Drawing.Point(335, 188);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(133, 26);
+            this.comboBox7.TabIndex = 17;
+            this.comboBox7.SelectedIndexChanged += new System.EventHandler(this.comboBox7_SelectedIndexChanged);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(487, 191);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(17, 18);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "?";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(184, 188);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(127, 18);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "- это животное";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label15.Location = new System.Drawing.Point(290, 57);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(0, 18);
+            this.label15.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 333);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(432, 188);
+            this.Name = "Form1";
+            this.Text = "Вопросы к сети";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ lab3/SemNet/Generator.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace setee
+{
+    class Generator
+    {
+        List<Entity> Entites = new List<Entity>();
+        List<Connection> connections = new List<Connection>();
+        public IReadOnlyList<Entity> entity => Entites;
+        public IReadOnlyList<Connection> connection => connections;
+
+        public Generator()
+        {
+
+            Entites.Add(new Entity("Животное"));//0
+            Entites.Add(new Entity("Назначение"));//1
+            Entites.Add(new Entity("Кому"));//2
+            Entites.Add(new Entity("Рыбка"));//3
+            Entites.Add(new Entity("Собака"));//4
+            Entites.Add(new Entity("Кошка"));//5
+            Entites.Add(new Entity("Ящерка"));//6
+            Entites.Add(new Entity("Попугай"));//7
+            Entites.Add(new Entity("Интерактивное"));//8
+            Entites.Add(new Entity("Декоративное"));//9
+            Entites.Add(new Entity("Себе"));//10
+            Entites.Add(new Entity("Ребенку"));//11
+            Entites.Add(new Entity("В офис"));//12
+            Entites.Add(new Entity("Шерсть"));//13
+            Entites.Add(new Entity("Наличие шерсти"));//14
+            Entites.Add(new Entity("Отсутствие шерсти"));//15
+            Entites.Add(new Entity("Готовность тратиться"));//16
+            Entites.Add(new Entity("Есть готовность тратиться"));//17
+            Entites.Add(new Entity("Нет готовности тратиться"));//18
+            Entites.Add(new Entity("Терпимость к шуму"));//19
+            Entites.Add(new Entity("Наличие терпимости к шуму"));//20
+            Entites.Add(new Entity("Отсутствие терпимости к шуму"));//21
+            Entites.Add(new Entity("Аллергия"));//22
+            Entites.Add(new Entity("Наличие аллергии"));//23
+            Entites.Add(new Entity("Отсутствие аллергии"));//24
+            Entites.Add(new Entity("Свободное время"));//25
+            Entites.Add(new Entity("Наличие свободного времени"));//26
+            Entites.Add(new Entity("Отсутствие свободного времени"));//27
+            Entites.Add(new Entity("Экзотичность"));//28
+            Entites.Add(new Entity("Экзотичное"));//29
+	        Entites.Add(new Entity("Не экзотичное"));//30
+            Entites.Add(new Entity("Либо себе, либо ребенку"));//31
+            Entites.Add(new Entity("Либо себе, либо ребенку, либо в офис"));//32
+            Entites.Add(new Entity("Либо себе, либо в офис"));//33
+
+
+
+
+
+            connections.AddRange(new List<Connection>()
+            {
+                new Connection("Влияет").SetEntitys(Entites[4], Entites[14]),
+                new Connection("Влияет").SetEntitys(Entites[5], Entites[14]),
+                new Connection("Влияет").SetEntitys(Entites[3], Entites[15]),
+                new Connection("Влияет").SetEntitys(Entites[6], Entites[15]),
+                new Connection("Влияет").SetEntitys(Entites[7], Entites[15]),
+                new Connection("Влияет").SetEntitys(Entites[3], Entites[20]),
+                new Connection("Влияет").SetEntitys(Entites[6], Entites[20]),
+                new Connection("Влияет").SetEntitys(Entites[3], Entites[21]),
+                new Connection("Влияет").SetEntitys(Entites[4], Entites[21]),
+                new Connection("Влияет").SetEntitys(Entites[5], Entites[21]),
+
+            });
+            connections.AddRange(new List<Connection>()
+            {
+                new Connection("Шерсть").SetEntitys(Entites[15], Entites[6]),
+                new Connection("Шерсть").SetEntitys(Entites[15], Entites[7]),
+                new Connection("Шерсть").SetEntitys(Entites[15], Entites[3]),
+                new Connection("Шерсть").SetEntitys(Entites[14], Entites[4]),
+                new Connection("Шерсть").SetEntitys(Entites[14], Entites[5]),
+
+            });
+
+
+            connections.AddRange(new List<Connection>()
+            {
+                new Connection("Тип животного").SetEntitys(Entites[29], Entites[6]),
+                new Connection("Тип животного").SetEntitys(Entites[29], Entites[7]),
+                new Connection("Тип животного").SetEntitys(Entites[30], Entites[3]),
+                new Connection("Тип животного").SetEntitys(Entites[30], Entites[4]),
+                new Connection("Тип животного").SetEntitys(Entites[30], Entites[5]),
+           
+            });
+
+
+
+            //  connections.AddRange(new List<Connection>()
+            //  {
+            //      new Connection("Шум").SetEntitys(Entites[20], Entites[4]),
+            //      new Connection("Шум").SetEntitys(Entites[20], Entites[7]),
+            //      new Connection("Шум").SetEntitys(Entites[21], Entites[3]),
+            //      new Connection("Шум").SetEntitys(Entites[21], Entites[5]),
+            //      new Connection("Шум").SetEntitys(Entites[21], Entites[6]),
+
+            //  });
+
+            connections.AddRange(new List<Connection>()
+            {
+                new Connection("AKO").SetEntitys(Entites[0], Entites[3]),
+                new Connection("AKO").SetEntitys(Entites[0], Entites[4]),
+                new Connection("AKO").SetEntitys(Entites[0], Entites[5]),
+                new Connection("AKO").SetEntitys(Entites[0], Entites[6]),
+                new Connection("AKO").SetEntitys(Entites[0], Entites[7]),
+                new Connection("AKO").SetEntitys(Entites[1], Entites[8]),
+                new Connection("AKO").SetEntitys(Entites[1], Entites[9]),
+                new Connection("AKO").SetEntitys(Entites[2], Entites[10]),
+                new Connection("AKO").SetEntitys(Entites[2], Entites[11]),
+                new Connection("AKO").SetEntitys(Entites[2], Entites[12]),
+                new Connection("AKO").SetEntitys(Entites[13], Entites[14]),
+                new Connection("AKO").SetEntitys(Entites[13], Entites[15]),
+                new Connection("AKO").SetEntitys(Entites[16], Entites[17]),
+                new Connection("AKO").SetEntitys(Entites[16], Entites[18]),
+                new Connection("AKO").SetEntitys(Entites[19], Entites[20]),
+                new Connection("AKO").SetEntitys(Entites[19], Entites[21]),
+                new Connection("AKO").SetEntitys(Entites[22], Entites[23]),
+                new Connection("AKO").SetEntitys(Entites[22], Entites[24]),
+                new Connection("AKO").SetEntitys(Entites[25], Entites[26]),
+		        new Connection("AKO").SetEntitys(Entites[25], Entites[27]),
+		        new Connection("AKO").SetEntitys(Entites[28], Entites[29]),
+		        new Connection("AKO").SetEntitys(Entites[28], Entites[30]),
+            });
+            connections.AddRange(new List<Connection>()
+            {
+                new Connection("Is_a").SetEntitys(Entites[24], Entites[4]),
+                new Connection("Is_a").SetEntitys(Entites[24], Entites[5]),
+                new Connection("Is_a").SetEntitys(Entites[23], Entites[3]),
+                new Connection("Is_a").SetEntitys(Entites[23], Entites[6]),
+                new Connection("Is_a").SetEntitys(Entites[23], Entites[7]),
+                //new Connection("Is_a").SetEntitys(Entites[30], Entites[26]),
+                //new Connection("Is_a").SetEntitys(Entites[29], Entites[22]),
+                //new Connection("Is_a").SetEntitys(Entites[29], Entites[23]),
+
+            });
+
+        }
+    }
+}
