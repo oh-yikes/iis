9abed155b2195405b1214c7acb67fe58f299560d
Darya Volokhova <71918075+oh-yikes@users.noreply.github.com>
2021-04-08 22:46:56 +0500

    Семантическая сеть

---
+++ lab_3/SemNet/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace setee
+{
+    class Entity
+    {
+        public string Name { get; set; }
+        public List<Connection> Connections { get; set; }
+
+        public Entity (string name)
+        {
+            Name = name;
+            Connections = new List<Connection>();
+        }
+
+        public void AddConnection(Connection con)
+        {
+            Connections.Add(con);
+        }
+
+        public void AddConnection(List<Connection> cons)
+        {
+            Connections.AddRange(cons);
+        }
+    }
+}
